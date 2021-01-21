4d63e5115046d840d1c8d245fb9c2dd799315510
fdu <1050329+fdu@users.noreply.github.com>
2021-01-21 22:46:36 +0100

    add equal and slash

--- keymaps.h
+++ keymaps.h
@@ -37,14 +37,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    _______,           _______, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
     KC_GRV,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_BSPC,           KC_TAB, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
     KC_BSPC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_TAB,           KC_MEH,  KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, TG(FUN),
-    KC_LSFT, LCTL_T(KC_Z),KC_X,KC_C,    KC_V,    KC_B,                                KC_N,    KC_M,    KC_COMM, KC_DOT,  RCTL_T(KC_SLSH), KC_RSFT,
+    KC_LSFT, LCTL_T(KC_Z),KC_X,KC_C,    KC_V,    KC_B,                                KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
     MO(SYMBOLS),_______,_______,KC_LEFT, KC_RGHT,  KC_LCTL,    KC_RCTL,   KC_UP,   KC_DOWN, KC_LBRC, KC_RBRC, MO(SYMBOLS),
                                           KC_SPC,  KC_BSPC, KC_LGUI,           KC_LALT,  KC_TAB,  KC_ENT
   ),
 
   [SYMBOLS] = LAYOUT_moonlander(
     VERSION, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______,           _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
-    _______, UC_A_GRAVE, UC_E_AIGU, UC_E_TREMA, UC_E_GRAVE, _______, _______,           _______, _______, _______, _______, _______, KC_ASTR, _______,
+    _______, UC_A_GRAVE, UC_E_AIGU, UC_E_TREMA, UC_E_GRAVE, _______, _______,           _______, _______, _______, _______, _______, KC_ASTR, KC_EQUAL,
     UC_A_AIGU, KC_AT, KC_DLR, UC_E_CIRC, _______, _______, _______,           _______, _______, _______, _______, _______, KC_PLUS, _______,
     _______, _______, _______, UC_C_CEDILLE, _______, _______,                             UC_N_TILDE, _______, _______, _______, KC_BSLS, _______,
     _______, _______, _______, _______, _______,         _______,            _______,          KC_PGUP, KC_PGDOWN, _______, _______, _______,
